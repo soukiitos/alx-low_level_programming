26dbd3d6b40168b824b71261f3df6d23328b057e
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-03-16 12:30:30 -0700

    print a quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - print a quote
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}

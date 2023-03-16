a6fd30086c557204216930bd6a3a9eef94433802
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-03-16 10:29:11 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 
+/**
+ *main - A C program that prints a line using the funcuion puts
+ *return : Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

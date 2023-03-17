0c577ff7fd3a27981d97a4f51af20531c75d88ab
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-03-17 11:31:13 -0700

    Print the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char A[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(A[26]);
+	}
+	putchar('\n');
+	return (0);
+}

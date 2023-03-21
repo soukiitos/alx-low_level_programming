7aed2ab7664d1d04190102c16b7243abaeb8b3ce
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-03-21 13:17:25 -0700

    Prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,12 +13,12 @@ int print_last_digit(int i)
 	r = i % 10;
 	if (r < 0)
 	{
-		_putchar(-r);
+		_putchar(-r + 48);
 		return (-r);
 	}
 	else
 	{
-		_putchar(r);
+		_putchar(r + 48);
 		return (r);
 	}
 }

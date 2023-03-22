9649ab92492badd1a6f4ac4dbe14b90d9e5f7e1e
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-03-21 21:10:23 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Print each new term in Fibonacci by adding the previous two terms
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	long int a, b, F1, F2;
+
+	a = 1;
+	b = 2;
+	F1 = F2 = 0;
+	while (F1 <= 4000000)
+	{
+		F1 = a + b;
+		a = b;
+		b = a;
+		if ((a % 2) == 0)
+		{
+			F2 = a + 1;
+		}
+	}
+	printf("%ld\n", F2);
+	return (0);
+}

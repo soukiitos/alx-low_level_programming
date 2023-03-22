78fb50222ee8edb38e49c72b178a616b8b92c4c4
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-03-21 20:15:27 -0700

    Print the n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -41,9 +41,6 @@ void print_times_table(int n)
 					_putchar((c / 100) + 48);
 					_putchar(((c / 10) % 10) + 48);
 					_putchar((c % 10) + 48);
-				}
-			}
+				}}
 			_putchar('\n');
-		}
-	}
-}
+		}}}

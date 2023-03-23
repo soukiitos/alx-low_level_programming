0ed9864f403bd20f9d7791ceccca5e4184e93e99
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-03-23 14:03:19 -0700

    Print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - Print a square
+ * @size: The size of the square
+ * return: 0 (Success)
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}

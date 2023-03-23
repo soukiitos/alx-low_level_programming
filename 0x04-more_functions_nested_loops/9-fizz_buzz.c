f97f91bb302df20f3e563ea2a92abdd3dbd9f92e
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-03-23 14:39:41 -0700

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Fizz Buzz test
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("Fizz Buzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		if (n != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}

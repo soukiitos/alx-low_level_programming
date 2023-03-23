e1f69eee12ebdc166789e40ae0bf91bce10f69cf
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-03-23 15:33:22 -0700

    Print an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - Print an integer
+ * @n: An integer
+ * Return: 0 (Success)
+ */
+void print_number(int n)
+{
+	unsigned int a = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		a = n;
+		_putchar('-');
+	}
+	a /= 10;
+	if (a != 0)
+	{
+		print_number(a);
+	}
+		_putchar((unsigned int) n % 10 + 48);
+}

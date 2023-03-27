1a332db386b2d1aac696b73e799f1e317e71f888
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-03-27 15:22:24 -0700

    Do not hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _atoi - Convert a string to an integer
+ * @s: THe string
+ *
+ * REturn: The integer
+ */
+int _atoi(char *s)
+{
+	int length = 0, digit = 0, i = 1;
+	unsigned int negi = 0;
+
+

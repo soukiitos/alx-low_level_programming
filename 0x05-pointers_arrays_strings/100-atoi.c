53534b25ec1f6afe330584f1f219d9d91a4c6263
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-03-27 16:27:07 -0700

    Convert Strings to Integers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,11 +3,26 @@
  * _atoi - Convert a string to an integer
  * @s: THe string
  *
- * REturn: The integer
+ * Return: The integer
  */
 int _atoi(char *s)
 {
-	int length = 0, digit = 0, i = 1;
-	unsigned int negi = 0;
+	int i = 1;
+	unsigned int n = 0;
 
-
+	do {
+		if (*s >= '0' && *s <= '9')
+		{
+			n = n * 10 + (*s - '0');
+		}
+		else if (*s == '-')
+		{
+			i = i * -1;
+		}
+		else if (n > 0)
+		{
+			break;
+		}
+	} while (*s++);
+	return (n * i);
+}

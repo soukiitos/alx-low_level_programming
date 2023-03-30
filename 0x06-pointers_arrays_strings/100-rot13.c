398b934f2b674082e40f54014498551545e0fa66
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-03-30 15:06:56 -0700

    Encode strings using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - Encode a string
+ * @s: The string
+ *
+ * Return: *s
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char d[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] = != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == d[j])
+			{
+				s[i] = r[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}

1c3e10a239cbd1f9396955d5efba959316fd25c7
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-03-30 15:51:34 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,25 +9,21 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] -= 32;
+	}
 	while (str[i])
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-		if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == ';' ||
-				str[i - 1] == '.' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == '"' ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				str[i - 1] == '{' ||
-				str[i - 1] == '}' ||
-				i == 0)
-			str[i] -= 32;
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+				str[i] == '!' || str[i] == '?' || str[i] == '\"' ||
+				str[i] == '(' || str[i] == ')' ||
+				str[i] == '{' || str[i] == '}')
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] -= 'a' - 'A';
+		}
 		i++;
 	}
 	return (str);

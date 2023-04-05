ad1091f9187528764260e77239c657e9d0ed62d2
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-04-05 16:48:19 -0700

    Return The Natural Square Root of a Number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,4 @@
-#include <stdio.h>
 #include "main.h"
-/**
- * _sqrt_recursion - return the natural square root of a number
- * @n: A number
- *
- * Return: 0
- */
-int _sqrt_recursion(int n)
-{
-	return (_Fsqrt(n, 0));
-}
 /**
  * _Fsqrt - Check if the square of a number exist
  * @n: A number
@@ -19,16 +8,21 @@ int _sqrt_recursion(int n)
  */
 int _Fsqrt(int i, int n)
 {
-	if ((i * i) > n)
-	{
-		return (-1);
-	}
-	else if ((i * i) == n)
-	{
-		return (i);
-	}
-	else
-	{
-		return (_Fsqrt(n, i + 1));
-	}
+        if (i * i == n)
+                return (i);
+	else if (i * i > n)
+                return (-1);
+        return (_Fsqrt(i + 1, n));
+}
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: A number
+ *
+ * Return: 0
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (_Fsqrt(1, n));
 }

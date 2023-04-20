5e3c712711a78b633f484ac83df385e1551d61c2
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-04-20 11:38:58 -0700

    Variadic numbers in C Programming Language

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * print_numbers - Print numbers
+ * @separator: The string to be printed between numbers
+ * @n: The number of integers passed to the function
+ * @...: A variable
+ *
+ * Return: 0
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list i;
+	unsigned int j = 0;
+
+	va_start(i, n);
+	while (j < n)
+	{
+		j++;
+		printf("%d", va_arg(i, int));
+		if (separator != NULL || j != (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(i);
+}

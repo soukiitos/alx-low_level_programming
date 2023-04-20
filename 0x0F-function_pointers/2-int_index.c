64da7dbce8fd8599747ddcb340064564df8cb865
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-04-19 17:05:09 -0700

    Search for an Integer, To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+/**
+ * int_index - Search for an integer
+ * @array: The array
+ * @size: The size of the array
+ * @cmp: The pointer to the function to be used to compare values
+ *
+ * Return: 0.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array != NULL || size > 0 || cmp != NULL)
+	{
+		for (; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+		}
+	}
+	else
+	{
+		return (-1);
+	}
+	return (-1);
+}

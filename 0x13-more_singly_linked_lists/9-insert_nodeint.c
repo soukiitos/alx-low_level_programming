a3ffa693a3bca90aa6d9e2f54a5e91e4ca4e2c6b
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-05-03 17:53:05 -0700

    Insert a New Node at a Given Position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	i->n = n;
 	if (idx == 0)
 	{
-		i->next =*head;
+		i->next = h;
 		*head = i;
 		return (i);
 	}
@@ -35,5 +35,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			h->next = i;
 			return (i);
 		}
-	return (NULL);
+	return (i);
 }

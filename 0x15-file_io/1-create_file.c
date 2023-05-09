07b1486409935614493680b433d317b53bda2ef1
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-05-08 17:13:13 -0700

    FILE I/O in C Programming Language

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,11 +14,11 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content)
+	if (!text_content)
 	{
 		text_content = "";
 	}
-	fn = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fn = open(filename, O_WRONLY | O_CREAT | O_TRUNC, '\200');
 	if (fn == -1)
 	{
 		return (-1);

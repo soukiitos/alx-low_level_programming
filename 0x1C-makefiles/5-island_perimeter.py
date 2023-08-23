030dc57af978ce1f383bebdb575f3ae82a6f4071
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-08-23 20:41:43 +0100

    Island Perimeter Task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    '''
+    Calculte the perimeter of the island described in the grid
+    grid is a list of list of integers
+    '''
+    perm = 0
+    r = len(grid)
+    c = len(grid[0])
+    for r in range(r):
+        for c in range(c):
+            if grid[r][c] == 1:
+                perm += 4
+                if r > 0 and grid[r - 1][c] == 1:
+                    perm -= 2
+                if c > 0 and grid[r][c - 1] == 1:
+                    perm -= 2
+    return perm

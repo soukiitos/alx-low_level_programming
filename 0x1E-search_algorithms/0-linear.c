bbf152c86ccf24140631a6b6a3e4d7d58bc26eaa
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-10 21:23:11 +0100

    Readme file and Linear search task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Search for a value in an array of integers
+ * using the Linear search algorithm
+ * @array: A pointer to the first element
+ * @size: The number of elements in array
+ * @value: The value to search for
+ *
+ * Return: 0
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array)
+	{
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+				return (i);
+		}
+	}
+	return (-1);
+}
